3f48968eb23c5eca14c94140febade3aa7fdbee2
unknown <>
2016-07-30 16:05:28 +0800

    By GitPush

--- proxy.js
+++ proxy.js
@@ -7,22 +7,22 @@ var hostEndPort = {};
 require('http').createServer(function(req,res){
 	var host = req.headers.host;
 	var nowtime = Math.round(new Date().getTime()/1000);
-	if(!hostEndPort[host] || !hostEndPort[host].Expires || hostEndPort[host].Expires < nowtime){
-		if(process.env[host]){
-			if(!hostEndPort[host]){
-
+	try{
+		if(!hostEndPort[host] || !hostEndPort[host].Expires || hostEndPort[host].Expires < nowtime){
+			if(process.env[host]){
+				if(!hostEndPort[host]){console.log("host:" + key + " endport >>> " + process.env[host])}
+				hostEndPort[host] = {EndPort:process.env[host],Expires:nowtime + 86400};
+			}else{
+				console.log('host:'+host+' endport not found!');
+				hostEndPort[host] = {EndPort:'',Expires:nowtime + 30};
 			}
-			hostEndPort[host] = {EndPort:process.env[host],Expires:nowtime + 86400};
-		}else{
-			console.log('host:'+host+' endport not found!');
-			hostEndPort[host] = {EndPort:'',Expires:nowtime + 30};
 		}
-	}
-	if(hostEndPort[host].EndPort){
-		proxy.web(req,res,{target:'http://'+hostEndPort[host].EndPort});
-	}else{
-		res.writeHead(200, {'Content-Type': 'text/html','WlniaoServer':'proxy-node'});
-		res.end("<html><head><title>Unknown Domain</title></head><body bgcolor=\"white\"><center><h1>Unknown Domain</h1></center><hr><center>proxy</center></body></html>");
-	}
+		if(hostEndPort[host].EndPort){
+			proxy.web(req,res,{target:'http://'+hostEndPort[host].EndPort});
+		}else{
+			res.writeHead(200, {'Content-Type': 'text/html','WlniaoServer':'proxy-node'});
+			res.end("<html><head><title>Unknown Domain</title></head><body bgcolor=\"white\"><center><h1>Unknown Domain</h1></center><hr><center>proxy</center></body></html>");
+		}
+	}catch(e){}
 }).listen(80);
 console.log("node proxy listening on port 80");
